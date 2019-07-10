2d7ea4f9f44dedf6171e05506f2a264c66274cbb
Maria Vanessa Moreno <47546293+baritoneplaya2@users.noreply.github.com>
2019-07-10 14:26:26 -0400

    Add files via upload

---
+++ story.js
@@ -0,0 +1,13 @@
+alert("Welcome to The Story Maker!");
+alert("Please write an example of each of the following words to write your very own story!");
+
+var answerAdjective = prompt("Adjective");
+var answerPluralNoun = prompt("Plural Noun");
+var answerPreciousMetal = prompt("Precious Metal");
+var answerTexture = prompt("Texture");
+
+alert("All done! Ready to read your story?");
+
+document.write("Here's the story of a " + answerAdjective + " lady who was bringing up three very lovely " + answerPluralNoun + ", all of them had hair of " + answerPreciousMetal + " like their mother, the youngest one in " + answerTexture + ".");
+
+//Here's the story of a ____ lady who was bringing up three very lovely ____, all of them had hair of ____ like their mother, the youngest one in ____.
